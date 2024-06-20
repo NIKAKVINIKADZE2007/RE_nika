1b6520b74bb476a4a83e9b816a805986e9b81c66
nikakvinikadze <nikakvinikadze2007@gmail.com>
2024-06-21 01:36:00 +0400

    adding responsive to products
    adding responsive

--- js/products.js
+++ js/products.js
@@ -3,13 +3,46 @@ let limit = 2;
 let container = document.getElementById('products-container');
 let prevbtn = document.getElementById('prev-button');
 let nextbtn = document.getElementById('next-button');
-displayproducts();
-function displayproducts() {
+
+fetch('./js/audio_products.json')
+  .then((res) => res.json())
+  .then((data) => {
+    displayproducts(data);
+    nextbtn.addEventListener('click', function () {
+      startpage++;
+      if (startpage == data.length / limit) {
+        nextbtn.classList.add('dispalaynone');
+        displayproducts(data);
+      } else {
+        displayproducts(data);
+      }
+      if (startpage > 1) {
+        prevbtn.classList.remove('dispalaynone');
+      }
+    });
+
+    prevbtn.addEventListener('click', function () {
+      startpage--;
+      if (startpage == 1) {
+        prevbtn.classList.add('dispalaynone');
+        displayproducts(data);
+      } else {
+        displayproducts(data);
+        prevbtn.classList.remove('dispalaynone');
+      }
+      if (startpage < data.length / limit) {
+        nextbtn.classList.remove('dispalaynone');
+        console.log('a');
+      }
+    });
+  });
+
+function displayproducts(data) {
   container.innerHTML = '';
-  let start = (startpage - 1) * 2;
+  let start = (startpage - 1) * limit;
   let end = start + limit;
 
-  let product = products.slice(start, end);
+  let product = data.slice(start, end);
   product.forEach((element) => {
     let div = document.createElement('div');
     let textdiv = document.createElement('div');
@@ -19,24 +52,30 @@ function displayproducts() {
     img.alt = 'img not found';
 
     let title = document.createElement('h2');
-    title.textContent = element.title;
+    title.textContent = element.name;
     title.classList.add('title');
 
+    let catagory = document.createElement('h2');
+    catagory.textContent = element.type;
+    catagory.classList.add('catagory');
+    console.log('rame');
+
     let decsription = document.createElement('p');
-    decsription.textContent = element.decsription;
+    decsription.textContent = element.description;
     decsription.classList.add('description');
 
-    console.log(element.id);
+    let seemore = document.createElement('button');
+    seemore.textContent = 'SEE PRODUCT';
+    seemore.classList.add('see-product');
+
     div.appendChild(img);
     textdiv.appendChild(title);
+    textdiv.appendChild(catagory);
     textdiv.appendChild(decsription);
+    textdiv.appendChild(seemore);
     div.appendChild(textdiv);
-
+    div.classList.add('products-div');
     container.appendChild(div);
+    prevbtn.classList.add('dispalaynone');
   });
 }
-
-nextbtn.addEventListener('click', function () {
-  startpage++;
-  displayproducts();
-});
